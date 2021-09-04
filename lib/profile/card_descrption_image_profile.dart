c4200c7b2852355cbf977208063f55be4c18dfa8
Norberto Aguilar Ramirez <norchilipeper@hotmail.com>
2021-09-04 17:49:31 -0600

    Pantalla del Perfil al 50%

--- lib/profile/card_descrption_image_profile.dart
+++ lib/profile/card_descrption_image_profile.dart
@@ -1,19 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:platzi_trips_app/floating_action_button_green.dart';
 
 class CardDescriptionImageProfile extends StatelessWidget {
-  String pathImage;
-  String title;
-  String description;
+  String namePlace;
+  String descriptionPlace;
   int stepsCount;
 
   CardDescriptionImageProfile(
-      this.pathImage, this.title, this.description, this.stepsCount);
+      this.namePlace, this.descriptionPlace, this.stepsCount);
 
   @override
   Widget build(BuildContext context) {
+    var formatNumber = new NumberFormat("###,###,###");
+
+    String stepsCountFormatted = formatNumber.format(stepsCount);
+
     final _title = Text(
-      title,
+      namePlace,
       textAlign: TextAlign.start,
       style: TextStyle(
           fontFamily: "Lato",
@@ -23,7 +27,9 @@ class CardDescriptionImageProfile extends StatelessWidget {
     );
 
     final _description = Text(
-      description,
+      descriptionPlace,
+      maxLines: 3,
+      overflow: TextOverflow.ellipsis,
       textAlign: TextAlign.start,
       style: TextStyle(
         fontFamily: "Lato",
@@ -33,7 +39,7 @@ class CardDescriptionImageProfile extends StatelessWidget {
     );
 
     final _stepsCount = Text(
-      stepsCount.toString(),
+      "Steps  $stepsCountFormatted",
       textAlign: TextAlign.start,
       style: TextStyle(
           fontFamily: "Lato",
@@ -43,20 +49,20 @@ class CardDescriptionImageProfile extends StatelessWidget {
     );
 
     final detailsCard = Container(
-        margin:
-            EdgeInsets.only(left: 10.0, top: 10.0, right: 10.0, bottom: 10.0),
+        margin: EdgeInsets.only(left: 10.0, right: 10.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          //mainAxisSize: MainAxisSize.max,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[_title, _description, _stepsCount],
         ));
 
     final card = Container(
-      height: 250.0,
-      width: 150.0,
+      alignment: Alignment.center,
+      height: 100.0,
+      width: 250.0,
       decoration: BoxDecoration(
-          image:
-              DecorationImage(fit: BoxFit.cover, image: AssetImage(pathImage)),
+          color: Colors.white,
           borderRadius: BorderRadius.all(Radius.circular(5.0)),
           shape: BoxShape.rectangle,
           boxShadow: <BoxShadow>[
@@ -69,7 +75,7 @@ class CardDescriptionImageProfile extends StatelessWidget {
     );
 
     return Stack(
-      alignment: Alignment(0.9, 1.1),
+      alignment: Alignment(0.9, 1.5),
       children: <Widget>[card, FloatingActionButtonGreen()],
     );
   }
--- lib/profile/card_image_with_description.dart
+++ lib/profile/card_image_with_description.dart
@@ -2,9 +2,39 @@ import 'package:flutter/material.dart';
 import 'card_descrption_image_profile.dart';
 
 class CardImageWithDescription extends StatelessWidget {
+  String pathImageOfPlace;
+  String namePlace;
+  String descriptionPlace;
+  int stepsCount;
+
+  CardImageWithDescription(this.pathImageOfPlace, this.namePlace,
+      this.descriptionPlace, this.stepsCount);
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    final card = Container(
+      height: 200.0,
+      //width: 300.0,
+      margin: EdgeInsets.only(top: 100.0, left: 20.0, right: 20.0),
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              fit: BoxFit.cover, image: AssetImage(pathImageOfPlace)),
+          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+          shape: BoxShape.rectangle,
+          boxShadow: <BoxShadow>[
+            BoxShadow(
+                color: Colors.black38,
+                blurRadius: 15.0,
+                offset: Offset(0.0, 7.0))
+          ]),
+    );
+
+    return Stack(
+      alignment: Alignment(0.0, 1.5),
+      children: <Widget>[
+        card,
+        CardDescriptionImageProfile(namePlace, descriptionPlace, stepsCount)
+      ],
+    );
   }
 }
